c2c2482fa62bdd1c83bba06ba5545501fcd0e35f
Gabriel Damico <gabrieldamico22@gmail.com>
2025-04-05 16:44:38 -0300

    feat: Criacao de header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { Header } from "@/components/header/header";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Hello world!</h1>
       <Button
         onClick={() => {
