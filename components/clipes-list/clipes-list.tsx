f07822d10e26e99b47570095949a6c42b4fa2ef7
Gabriel Damico <gabrieldamico22@gmail.com>
2025-04-05 21:32:19 -0300

    chore: adiciona o dotenv

--- components/clipes-list/clipes-list.tsx
+++ components/clipes-list/clipes-list.tsx
@@ -3,9 +3,10 @@ import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export default async function ClipesList() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const [response, data] = await Promise.all([
     fetch(
-      "http://localhost:3000/api/proxy?url=https://cdn.discordapp.com/attachments/1145570523330379917/1358101550265794880/clipe.mp4?ex=67f29e21%26is=67f14ca1%26hm=6d8c26205295c7fcd04aca932fbd008fec5ee08ea89627bebd43e7ee73627fb1%26",
+      `${baseUrl}/api/proxy?url=https://cdn.discordapp.com/attachments/1145570523330379917/1358101550265794880/clipe.mp4?ex=67f29e21%26is=67f14ca1%26hm=6d8c26205295c7fcd04aca932fbd008fec5ee08ea89627bebd43e7ee73627fb1%26`,
     ),
     fetch("https://api.github.com/users/arawns1"),
   ]);
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const nextJest = require("next/jest");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const dotenv = require("dotenv");
 
+dotenv.config({
+  path: ".env.development",
+});
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
@@ -10,6 +15,7 @@ const createJestConfig = nextJest({
 const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "<rootDir>"],
 };
 
 module.exports = createJestConfig(config);
