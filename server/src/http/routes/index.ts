30e199a3bab1514c74645f111da83baaf86f0972
Gabriel Damico <gabrieldamico22@gmail.com>
2025-04-06 17:18:42 -0300

    feat: Criacao de rota proxy

--- server/src/http/routes/index.ts
+++ server/src/http/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { helloWorld } from "./hello-world";
+import { getDiscordVideo } from "./get-discord-video";
 
 export default async function routes(app: FastifyInstance) {
   app.register(helloWorld);
+  app.register(getDiscordVideo);
 }
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import routes from "./routes";
 
 const app = fastify();
 
+app.register(cors, {
+  origin: "*",
+});
+
 app.register(routes, { prefix: "/api" });
 
-app.listen({ port: 3000 }).then(() => {
-  console.log("🚀 HTTP server running!");
+app.listen({ port: 3100 }).then(() => {
+  console.log("🚀 HTTP server running on http://localhost:3100/api");
 });
